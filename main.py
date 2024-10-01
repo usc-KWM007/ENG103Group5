6b6f83c7073eccbf704a6936a6f2389d4c4eaf02
usc-KWM007 <KWM007@student.usc.edu.au>
2024-10-01 11:30:51 +1000

    Turning on Lights with Flask

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
-
+from gpiozero import LED
 app = Flask(__name__)
+lightled = LED(21)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/Light/<state>')
+def light_state(state):
+	if state == 'on':
+		lightled.on()
+		message = "LED is on"
+	elif state == 'off':
+		lightled.off()
+		message = "Led is off"
+	else:
+		message = "Invalid State"
+	return render_template('index.html', message=message)#
+
