640f58bee03a55d25a767fb89eaa9cad874cd4fa
usc-KWM007 <KWM007@student.usc.edu.au>
2024-10-05 17:00:32 +1000

    added button support for turning on LED and a refresh button to update website status

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 from flask import Flask, render_template
-from gpiozero import LED
+from gpiozero import LED, Button
+import sys
 
 app = Flask(__name__)
 
 lightled = LED(21)
+lightbutton = Button(6)
+
+def buttonpress():
+	print("buttonpressed", file=sys.stderr)
+	lightled.off() if lightled.is_lit else lightled.on()
+	return
+
+lightbutton.when_pressed =  buttonpress
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	message = "lights are on" if lightled.is_lit else "Lights are off"
+	return render_template('index.html', message=message)
 
 @app.route('/Light/<state>')
 def light_state(state):
 	if state == 'on':
 		lightled.on()
-		message = "LED is on"
+		message = "Lights are on"
 	elif state == 'off':
 		lightled.off()
-		message = "Led is off"
+		message = "Lights are off"
 	else:
 		message = "Invalid State"
-	return render_template('index.html', message=message)#
+	return render_template('index.html', message=message)
 
 if __name__=="__main__":
 	while True:
     		try:
         		if __name__ == "__main__":
             			app.run(host='0.0.0.0', port=80)
-    		except KeyboardInterrupt:
+    		except KeyboardInterrupt: #clean up GPIO
         		print("Ending program")
