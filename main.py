a0c206a46e36672aa2028529356360e5ac8f107d
usc-KWM007 <KWM007@student.usc.edu.au>
2024-10-06 01:52:01 +1000

    Added raindrop support and temperature support, RGB lights coming soon

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from flask import Flask, render_template
-from gpiozero import LED, Button, DistanceSensor
+from gpiozero import LED, Button, DistanceSensor, InputDevice
 import sys
 from time import sleep
 import threading
 import statistics
+import adafruit_dht
+import board
 
 app = Flask(__name__)
 
 lightled = LED(21)
 lightbutton = Button(6)
-
+rainsensor = InputDevice(17)
+rainled = LED(20)
 distancesensor = DistanceSensor(echo=22,trigger=27)
+dht_device = adafruit_dht.DHT11(board.D23)
 
-def distancesensorActivity():
+def sensorActivity():
 	#arrary for holding measurements
 	measurements=[]
 	while True:
+		#check rain sensor
+		if rainsensor.is_active:
+			rainled.off()
+		else:
+			rainled.on()
+		try:
+			temperature_c = dht_device.temperature
+			temperature_f = temperature_c * (9 / 5) + 32
+
+			humidity = dht_device.humidity
+
+			print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
+		except RuntimeError as err:
+			print(err.args[0])
 		measurement = distancesensor.distance*100
 		#avoid adding connection errors to array
 		if isinstance(measurement, float):
@@ -36,6 +54,7 @@ def distancesensorActivity():
 		print(measurement, file=sys.stderr)
 		print(measurements)
 		sleep(1)
+
 
 def buttonpress():
 	#trigged by light button
@@ -67,7 +86,7 @@ if __name__=="__main__":
         		if __name__ == "__main__":
             			t1 = threading.Thread(target = app.run, kwargs=dict(host='0.0.0.0', port=80)).start()
             			#keep distance checking on seperate thread due to looping
-            			t2 = threading.Thread(target = distancesensorActivity).start()
+            			t2 = threading.Thread(target = sensorActivity).start()
             			
             			
     		except KeyboardInterrupt: #clean up GPIO
